0bc4ec0898c882c8359d6e14abfc7adaace5ef8e
Jose Murcia <josemurciabelmonte@Joses-MacBook-Pro.local>
2023-01-12 21:44:52 +0100

    program that asks for a number and transforms it into morse

---
+++ Tema5/Semana15/Semana15/src/Morse.java
@@ -0,0 +1,101 @@
+import com.sun.source.doctree.SystemPropertyTree;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Morse {
+    /*
+        *****************************************************************************
+        *    IMPORTANTE:                                                            *
+        *    Para un correcto visionado del programa, recomiendo ejecutarlo         *
+        *    directamente desde la terminal del sistema y no desde la terminal      *
+        *    integrada en el IDE, dado que normalmente estas terminales no tienen   *
+        *    las funciones de refresco que utilizo aquí.                            *
+        *****************************************************************************
+        *
+     */
+    private final String[] MORSE_CODIFICATION =
+            {"_____",   //Equivalente a 0
+            ".____",    //Equivalente a 1
+            "..___",    //Equivalente a 2
+            "...__",    //Equivalente a 3
+            "...._",    //Equivalente a 4
+            ".....",    //Equivalente a 5
+            "_....",    //Equivalente a 6
+            "__...",    //Equivalente a 7
+            "___..",    //Equivalente a 8
+            "____."};   //Equivalente a 9
+    public static void main(String[] args) {
+        Morse morse = new Morse();
+        morse.executeProgram();
+    }
+
+    private void executeProgram() {
+        String osName = getOperatingSystemName();
+        char userWantsToContinue;
+        String morseNumber="";
+        int numberToConvert;
+        do {
+            clearConsole(osName);
+            showHeader();
+            numberToConvert = Integer.parseInt(askForANumberToConvert());
+            morseNumber = convierteEnMorse(numberToConvert);
+            System.out.println("El número introducido ha sido el " + numberToConvert + " , el cual, tranformado a morse es :" +
+                    "\n" + morseNumber);
+            System.out.print("\n\n¿Quieres convertir otro número? (pulsa 'y' para repetir o cualquier tecla para salir.): ");
+            userWantsToContinue = getUserInput().toLowerCase().charAt(0);
+        }while(userWantsToContinue == 'y');
+        System.out.println("\n\n¡¡Hasta luego!!");
+
+    }
+
+    private void showHeader(){
+        System.out.println(".___  ___.   ______   .______          _______. _______      ______   ______    _______   __   _______  __    ______     ___   .___________.  ______   .______      \n" +
+                "|   \\/   |  /  __  \\  |   _  \\        /       ||   ____|    /      | /  __  \\  |       \\ |  | |   ____||  |  /      |   /   \\  |           | /  __  \\  |   _  \\     \n" +
+                "|  \\  /  | |  |  |  | |  |_)  |      |   (----`|  |__      |  ,----'|  |  |  | |  .--.  ||  | |  |__   |  | |  ,----'  /  ^  \\ `---|  |----`|  |  |  | |  |_)  |    \n" +
+                "|  |\\/|  | |  |  |  | |      /        \\   \\    |   __|     |  |     |  |  |  | |  |  |  ||  | |   __|  |  | |  |      /  /_\\  \\    |  |     |  |  |  | |      /     \n" +
+                "|  |  |  | |  `--'  | |  |\\  \\----.----)   |   |  |____    |  `----.|  `--'  | |  '--'  ||  | |  |     |  | |  `----./  _____  \\   |  |     |  `--'  | |  |\\  \\----.\n" +
+                "|__|  |__|  \\______/  | _| `._____|_______/    |_______|    \\______| \\______/  |_______/ |__| |__|     |__|  \\______/__/     \\__\\  |__|      \\______/  | _| `._____|");
+    }
+
+    private String getOperatingSystemName(){
+        return System.getProperty("os.name");
+    }
+
+    private void clearConsole(String osName){
+        try {
+            if (osName.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush(); ;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        return userInput;
+    }
+
+    private String askForANumberToConvert(){
+        System.out.print("\n\nIntroduce un número entero para convertir a Morse: ");
+        return getUserInput();
+
+    }
+    private String convierteEnMorse (int numberToConvert){
+        int partOfTheNumber;
+        if (numberToConvert < 10) {
+            return MORSE_CODIFICATION[numberToConvert];
+        } else {
+            partOfTheNumber = numberToConvert % 10;
+            numberToConvert /= 10;
+            return convierteEnMorse(numberToConvert)+MORSE_CODIFICATION[partOfTheNumber];
+        }
+    }
+
+}
