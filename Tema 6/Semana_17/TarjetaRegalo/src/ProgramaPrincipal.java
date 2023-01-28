c630965b918a0626301f4dae9042007608b17f01
Jose Murcia <josemurciabelmonte@Joses-MacBook-Pro.local>
2023-01-28 13:08:45 +0100

    Irene's project 'TarjetaRegalo' has been created using the way that she asked for

---
+++ Tema 6/Semana_17/TarjetaRegalo/src/ProgramaPrincipal.java
@@ -0,0 +1,33 @@
+/*
+    -Implementa la clase TarjetaRegalo.
+    -Cuando se crea una nueva tarjeta, se le da un saldo y se asigna de forma automática un número de 5 dígitos.
+    -Si se intenta gastar más dinero del que tiene la tarjeta, se debe mostrar un mensaje de error.
+    -Dos tarjetas regalo se pueden fusionar creando una nueva tarjeta con la suma del saldo que tenga cada una y
+        un nuevo número aleatorio de 5 cifras.
+    -Al fusionar dos tarjetas en una, las dos tarjetas originales se quedarían con 0 € de saldo.
+ */
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        TarjetaRegalo t1 = new TarjetaRegalo(100);
+
+        TarjetaRegalo t2 = new TarjetaRegalo(120);
+
+        t1.gasta(45.90);
+        t2.gasta(5);
+        t2.gasta(200);
+        t1.gasta(3.55);
+        System.out.println(t1.comprobarSaldo());
+        System.out.println(t2.comprobarSaldo());
+        TarjetaRegalo t3 = new TarjetaRegalo(t1.fusionaCon(t2));
+        System.out.println(t1.comprobarSaldo());
+        System.out.println(t2.comprobarSaldo());
+        System.out.println(t3.comprobarSaldo());
+        /*
+            *** NOTA: He mantenido como comentario el código necesario para comprobar que el número generado para la tarjeta funciona. ***
+
+            System.out.println("El número de la tarjeta 1 es: " + t1.numeroTarjeta);
+            System.out.println("El número de la tarjeta 2 es: " + t2.numeroTarjeta);
+            System.out.println("El número de la tarjeta 3 es: " + t3.numeroTarjeta);
+         */
+    }
+}
---
+++ Tema 6/Semana_17/TarjetaRegalo/src/TarjetaRegalo.java
@@ -0,0 +1,39 @@
+public class TarjetaRegalo {
+    double saldo;
+    String numeroTarjeta = generarNumeroTarjeta();
+
+    public TarjetaRegalo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String generarNumeroTarjeta() {
+        String zeroesAddedAtTheBeggining = "";      //Se añadirán tantos 0 a la cadena como cifras le falten a "numeroTarjeta" para tener longitud 5.
+        this.numeroTarjeta = Integer.toString((int) (Math.random() * (99999 - 0 + 1) + 0));
+        if (this.numeroTarjeta.length() < 5) {
+            for (int i = this.numeroTarjeta.length(); i < 5; i++) {
+                zeroesAddedAtTheBeggining += "0";
+            }
+        }
+        return zeroesAddedAtTheBeggining + this.numeroTarjeta;
+    }
+
+    public double comprobarSaldo() {
+        return this.saldo;
+    }
+
+    public void gasta(double spentAmount) {
+        if (this.saldo < spentAmount) {
+            System.out.println("No tiene saldo suficiente para gastar " + spentAmount + "€");
+        } else {
+            this.saldo -= spentAmount;
+        }
+    }
+
+    public double fusionaCon(TarjetaRegalo tarjeta) {
+        double totalBalance = tarjeta.saldo + this.saldo;
+        tarjeta.saldo = 0;
+        this.saldo = 0;
+        return totalBalance;
+    }
+
+}
