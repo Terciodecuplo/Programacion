abb021207759fa55de80785b3d6a773df0d81c69
Jose Murcia <josemurciabelmonte@gmail.com>
2023-03-08 20:49:24 +0100

    exercise finished. The output equals the provided

--- Tema 9/Semana23Parte2/src/main/java/market/Carrito.java
+++ Tema 9/Semana23Parte2/src/main/java/market/Carrito.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 
 public class Carrito {
     private final ArrayList<Elemento> listaElementos = new ArrayList<>();
-    public void agrega(Elemento elemento){
+
+    public void agrega(Elemento elemento) {
         this.listaElementos.add(elemento);
     }
-    public int numeroDeElementos(){
+
+    public int numeroDeElementos() {
         return this.listaElementos.size();
     }
-    public double importeTotal(){
+
+    public double importeTotal() {
         double importe = 0;
-        for(Elemento elemento : listaElementos){
-           importe += elemento.getPrecio() * elemento.getUnidades();
+        for (Elemento elemento : listaElementos) {
+            importe += elemento.getPrecio() * elemento.getUnidades();
         }
         return importe;
     }
 
+    @Override
+    public String toString() {
+        String messageHeader = "Contenido del carrito\n=====================\n";
+        String showElementContent = "";
+        for (Elemento elemento : this.listaElementos) {
+            showElementContent += elemento.getNombreProducto();
+            showElementContent +=
+                    " PVP: " + String.format("%.2f", elemento.getPrecio()) +
+                    " Unidades: " + elemento.getUnidades() +
+                    " Subtotal: " + String.format("%.2f", (elemento.getUnidades() * elemento.getPrecio())) + "\n";
+        }
+        return messageHeader + showElementContent;
+    }
 }
